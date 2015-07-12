fa1401b9e18e6f28e274a79c9f149be405e9e8d2
abhik1368 <abhik1368@gmail.com>
2015-07-11 21:56:16 -0500

    Updated codes
    Update controls,data,plots,tables,server and ui r codes

--- server.R
+++ server.R
@@ -1,15 +1,16 @@
 library(shiny)
-library(openfda)
+source("openfda.R")
 library(ggplot2)
 library(scales)
 library(dplyr)
 library(reshape2)
 library(ggthemes)
+library(shinythemes)
 shinyServer(function(input, output) {
-
-  source('controls.R', local = TRUE)
-  source('data.R', local = TRUE)
-  source('plots.R', local = TRUE)
-  source('tables.R', local = TRUE)
-
+    
+    source('controls.R', local = TRUE)
+    source('data.R', local = TRUE)
+    source('plots.R', local = TRUE)
+    source('tables.R', local = TRUE)
+    
 })
